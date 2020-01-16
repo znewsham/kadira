bf9e736eeb7e7f6396ff47beb1628497d4737760
Zack Newsham <zack_newsham@yahoo.co.uk>
2020-01-16 14:26:56 -0500

    version bump

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   "summary": "Performance Monitoring for Meteor",
-  "version": "2.31.3",
+  "version": "2.31.4",
   "git": "https://github.com/znewsham/kadira.git",
   "name": "znewsham:kadira"
 });
