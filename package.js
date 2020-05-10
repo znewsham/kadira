bb0199e60100b371dd06d7511cbff2b0c7ca21d7
Zack Newsham <zack_newsham@yahoo.co.uk>
2020-05-10 10:38:24 -0400

    make work with 1.10

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   "summary": "Performance Monitoring for Meteor",
-  "version": "2.31.4",
+  "version": "2.31.5",
   "git": "https://github.com/znewsham/kadira.git",
   "name": "znewsham:kadira"
 });
